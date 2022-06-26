a9fe6c1ce7cd9ef1ac45d0d03e6a000eac113e75
fsy412 <fsy412@163.com>
2022-06-26 10:32:26 +0800

    project commit

---
+++ orderbook/src/main.rs
@@ -0,0 +1,28 @@
+use marine_rs_sdk::marine;
+use marine_rs_sdk::module_manifest;
+
+module_manifest!();
+
+pub fn main() {}
+
+#[marine]
+pub fn greeting(name: String) -> String {
+    format!("Hi, {}", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use marine_rs_sdk_test::marine_test;
+
+    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
+    fn empty_string(greeting: marine_test_env::greeting::ModuleInterface) {
+        let actual = greeting.greeting(String::new());
+        assert_eq!(actual, "Hi, ");
+    }
+
+    #[marine_test(config_path = "../Config.toml", modules_dir = "../artifacts")]
+    fn non_empty_string(greeting: marine_test_env::greeting::ModuleInterface) {
+        let actual = greeting.greeting("name".to_string());
+        assert_eq!(actual, "Hi, name");
+    }
+}
